838aaadc584892b02fa9883ffc1d264b55a62501
bmanara <bmacraze@gmail.com>
2024-12-20 01:51:32 +0800

    Attempt 10-2

--- 10/main.py
+++ 10/main.py
@@ -41,6 +41,46 @@ def checkRoute(currElevation, currPos, nextElevation, visited, dd):
     return resultList
 
 
+def partTwo():
+    # Generate all directions into tuples
+    dd = generateDirections()
+    
+    result = 0
+    for i in range(row):
+        for j in range(col):
+            r = checkDistinctRoute(int(lines[i][j]), (i, j), 0, [], dd)
+            result += len(r)
+
+    return result
+
+
+def checkDistinctRoute(currElevation, currPos, nextElevation, visited, dd):
+    if (currElevation != nextElevation):
+        return visited
+
+    if (nextElevation == 9 and currElevation == 9):
+        visited.append(currPos)
+        return visited
+
+    resultList = []
+    for d in dd:
+        nextPos = list(currPos)
+        nextPos[0] += d[0] 
+        nextPos[1] += d[1]
+        nextPos = tuple(nextPos)
+        if not (0 <= nextPos[0] < row and 0 <= nextPos[1] < col):
+            continue
+
+        newVisited = checkRoute(int(lines[nextPos[0]][nextPos[1]]), 
+                                nextPos, 
+                                nextElevation + 1, 
+                                visited, 
+                                dd)
+        for v in newVisited:
+            resultList.append(v)
+
+    return resultList
+
 
 
 def generateDirections():
@@ -54,9 +94,10 @@ def generateDirections():
 
 
 if __name__ == '__main__':
-    with open("input.txt") as f:
+    with open("test.txt") as f:
         lines = f.read().strip().split("\n")
     row = len(lines)
     col = len(lines[0])
 
     print(partOne())
+    print(partTwo())
